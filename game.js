506a261f355c84e8b228a4865817df095cbb51e8
DEEP-17 <pateldeep17042004@gmail.com>
2025-03-16 17:10:13 +0530

    silly

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-const socket = io("http://localhost:3000");
+const socket = io("https://chess-game-backend-z158.onrender.com");
      var board = null;
      var count=0;
     var game = new Chess();
